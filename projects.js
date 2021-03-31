420892ef6a4d6d385d088eadf0d08f98ae98b1e5
mario130 <mario.yonan.work@gmail.com>
2021-03-31 23:56:27 +0200

    updated api

--- projects.js
+++ projects.js
@@ -3,9 +3,10 @@ export const projects = [
     id: 1,
     title: "Portfolio project",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
-    stack: ["HTML", "CSS", "JS"],
+    stack: ["React", "Next", "Tailwind"],
     website: "www.google.com",
     repo: "www.google.com",
+    imageUrl: "https://i.imgur.com/Itfn54U.png"
   },
   {
     id: 2,
