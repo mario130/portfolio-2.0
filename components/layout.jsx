6423bf4a14285600e42ca0d579368a4495fd6f7f
mario130 <mario.yonan.work@gmail.com>
2021-03-31 22:15:05 +0200

    persistent dark mode

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,14 +1,32 @@
 import Footer from "./footer";
 import Nav from "./nav";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Layout({ children }) {
   const [isDarkMode, setDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if (localStorage.getItem('theme') === 'dark') {
+      setDarkMode(true)
+    } else {
+      setDarkMode(false)
+    }
+  }, [])
+
+  const switchDarkMode = () => {
+    if (isDarkMode) {
+      setDarkMode(false);
+      localStorage.setItem("theme", "light");
+    } else {
+      setDarkMode(true);
+      localStorage.setItem("theme", "dark");
+    }
+  }
   
   return (
     <div className={`${isDarkMode ? 'dark' : ''}`}>
       <div className="dark-wrapper dark:bg-dark-bgPrimary text-white">
-        <Nav isDarkMode={isDarkMode} setDarkMode={setDarkMode} />
+        <Nav isDarkMode={isDarkMode} switchDarkMode={switchDarkMode} />
         <div className="container mx-auto px-6 text-text dark:text-gray-200 my-16">
           {children}
         </div>
--- components/nav.jsx
+++ components/nav.jsx
@@ -8,7 +8,7 @@ const Nav = (props) => {
     <div className="border-t-8 border-primary mb-14 text-text dark:text-gray-200">
     <nav className="container m-auto flex justify-between mt-6 px-6">
       <div className="logo">Mario.</div>
-      <ul className="links flex space-x-4 items-center">
+      <ul className="links flex md:space-x-4 items-center">
         <li>
           <Link href="/">
             {/* <a className="border-b-2 border-primary p-2">Home</a> */}
@@ -25,8 +25,8 @@ const Nav = (props) => {
             <a className="border-b-2 border-none p-2">About</a>
           </Link>
         </li>
-        <li>
-          <span className="border-b-2 border-none cursor-pointer" onClick={()=>props.setDarkMode(!props.isDarkMode)}>
+        <li className="ml-2 md:ml-0">
+          <span className="border-b-2 border-none cursor-pointer" onClick={()=>props.switchDarkMode()}>
             {props.isDarkMode 
             ? <svg className="w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
