c4e606644cbfaaf0e4629d7e2f494a75feadca8d
mario130 <mario.yonan.work@gmail.com>
2021-03-31 23:24:55 +0200

    add api -2

---
+++ projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Portfolio project",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
+    stack: ["HTML", "CSS", "JS"],
+    website: "www.google.com",
+    repo: "www.google.com",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
+    stack: ["HTML", "CSS", "JS"],
+    website: "www.google.com",
+    repo: "www.google.com",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
+    stack: ["HTML", "CSS", "JS"],
+    website: "www.google.com",
+    repo: "www.google.com",
+  },
+]
