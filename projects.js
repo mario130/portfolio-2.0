e8850e12084405a7b7cf0dfcd788c5e2054d81cf
mario130 <mario.yonan.work@gmail.com>
2021-04-01 21:02:44 +0200

    updated api

--- projects.js
+++ projects.js
@@ -1,27 +1,31 @@
 export const projects = [
   {
     id: 1,
-    title: "Portfolio project",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
+    title: "Portfolio",
+    description: "My second portfolio website, but this time with a library and SSG which I'm starting to get the hang of.",
     stack: ["React", "Next", "Tailwind"],
-    website: "www.google.com",
-    repo: "www.google.com",
+    website: "https://portfolio-mario130.vercel.app/",
+    repo: "",
+    summary: "Built using Tailwind with React which was an excellent combination that I enjoyed a lot, it was a good training for Next.  The site still needs more work but I like the results so far.. ",
     imageUrl: "https://i.imgur.com/Itfn54U.png"
   },
   {
     id: 2,
-    title: "Project 2",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
-    stack: ["HTML", "CSS", "JS"],
-    website: "www.google.com",
-    repo: "www.google.com",
+    title: "Marketify",
+    description: "Yet another Ecommerce website, this time for furniture, first experience in hashing passwords and using jwt.",
+    stack: ["Angular", "Mongo", "Express", "API", "Firebase"],
+    website: "https://marketify-130.web.app/",
+    repo: "",
+    summary: "TEAM PROJECT - Although I've did most of the work excluding the homepage, I've built the database and the express server from scratch and added the authentication system, used Firebase for hosting.",
+    imageUrl: "https://i.imgur.com/cgfxjyR.png"
   },
   {
     id: 3,
     title: "Project 3",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores debitis delectus voluptatibus quasi cupiditate tenetur.",
     stack: ["HTML", "CSS", "JS"],
-    website: "www.google.com",
-    repo: "www.google.com",
+    website: "https://www.google.com",
+    repo: "https://www.google.com",
+    summary: "this is a test summary!",
   },
 ]
