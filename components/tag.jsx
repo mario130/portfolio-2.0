fd527d521f343061fb25c36a758892b50abb9231
mario130 <mario.yonan.work@gmail.com>
2021-04-05 10:04:59 +0200

    add tag hover effect

--- components/tag.jsx
+++ components/tag.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-const Tag = ({skill, i, noMarginLeft}) => (
+const Tag = ({skill, noMarginLeft, i}) => (
   <Link href={`/tag/${skill}`}>
     <a className={`stack-tag ${noMarginLeft ? "stack-no-ml": ""}`} key={i}>#{skill}</a>
   </Link>
